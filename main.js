1434208da8c73152674fadaa0e1ea6bf51fb723e
Michael DeVries <michael.devries16095@gmail.com>
2022-10-20 15:23:13 -0500

    troubleshooting drag handlers

--- main.js
+++ main.js
@@ -46,7 +46,6 @@ document.getElementById("listname").addEventListener("click", whatWasClicked); /
 // determine the action taken on the click
 function whatWasClicked(e) {
   if (e.target.tagName == "SPAN") deleteli(e);
-  if (e.target.className == "moveSymbol") onDragStart(e);
 }
 
 // removes specified task
@@ -62,8 +61,12 @@ let draggingEle;
 let x = 0;
 let y = 0;
 
+let placeholder;
+let isDraggingStarted = false;
+
 const mouseDownHandler = function (e) {
-  draggingEle = e.target;
+  draggingEle = e.target.parentNode;
+  console.log({draggingEle})
 
   // calculate the mouse position
   const rect = draggingEle.getBoundingClientRect();
@@ -80,6 +83,40 @@ const mouseMoveHandler = function (e) {
   draggingEle.style.postion = "absolute";
   draggingEle.style.left = "${e.pageX - x}px";
   draggingEle.style.top = "${e.pageY - y}px";
+
+  const draggingRect = draggingEle.getBoundingClientRect();
+
+  if (!isDraggingStarted) {
+    // update the flag
+    isDraggingStarted = true;
+
+    // Let the placeholder take the height of dragging element
+    // So the next element won't move up
+    placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    draggingEle.parentNode.insertBefore(
+      placeholder, draggingEle.nextSibling
+    );
+
+    // Set the placeholder's height
+    placeholder.style.height = '${draggingRect.height}px';
+
+    const prevEle = draggingEle.previousElementSibling;
+    const nextEle = draggingEle.nextelementSibling;
+
+    // user moves item to the top
+    if (prevEle && isAbove(draggingEle, prevEle)) {
+      swap(placeholder, draggingEle);
+      swap(placeholder, prevEle);
+      return;
+    }
+
+    // user moves the dragging element to the bottom
+    if (nextEle && isAbove(nextEle, draggingEle)) {
+      swap(nextEle, placeholder);
+      swap(nextEle, draggingEle);
+    }
+  }
 };
 
 const mouseUpHandler = function () {
@@ -95,13 +132,40 @@ const mouseUpHandler = function () {
   // remove listeners
   document.removeEventListener("mousemove", mouseMoveHandler);
   document.removeEventListener("mouseup", mouseUpHandler);
+
+  // remove the placeholder
+  placeholder && placeholder.parentNode.removeChild(placeholder);
+  // reset the flag
+  isDraggingStarted = false;
 };
 
+const isAbove = function (nodeA, nodeB) {
+  // Get the bounding rectangle of nodes
+  const rectA = nodeA.getBoundingClientRect();
+  const rectB = nodeB.getBoundingClientRect();
+
+  return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
+}
+
+const swap = function (nodeA, nodeB) {
+  const parentA = nodeA.parentNode;
+  const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+  // move 'nodeA' to before the 'nodeB'
+  nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+  // move 'nodeB' to before the sibling of 'nodeA'
+  parentA.insertBefore(nodeB, siblingA);
+}
+
 // query out list
-const list = document.getElementsById("listname");
+const list = document.getElementById("listname");
 
 // query all items
-[].slice.call(list.querySelectorAll(".draggable")).forEach();
+[].slice.call(list.querySelectorAll(".draggable")).forEach(function (item) {
+  item.addEventListener("mousedown", mouseDownHandler);
+});
+
 
 // EDIT ALREADY CREATED TASKS
 
